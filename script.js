8cf50de3b1e0273a0c86512c9dc9bd99b70fd414
Cah Bantul <fardan.nozami@gmail.com>
2021-03-03 11:03:22 +0700

    latian arrow function tahap pertama

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// arrow function
+// function expression
+// const tampilNama = function (name) {
+//   console.log(`halo ${name}`);
+// };
+// tampilNama("Nozami");
+
+// Arrow Function dan implisit return
+// const tampilNama = (nama) => `halo ${nama}`;
+// console.log(tampilNama("Fardan"));
+
+let animal = ["chicken", "dog", "lion", "monkey"];
+
+// function expression
+// let jumlahHuruf = animal.map(function (nama) {
+//   return nama.length;
+// });
+
+// console.log(jumlahHuruf);
+
+// Arrow Function
+let jumlahHuruf = animal.map((nama) => ({ nama, JmlHuruf: nama.length }));
+console.table(jumlahHuruf);
